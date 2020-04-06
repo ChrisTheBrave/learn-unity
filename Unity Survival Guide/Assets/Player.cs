22c3bb63b5b6641b6f8b88d5e19929f5d99a00c3
Christine Ash <christine.ash7@gmail.com>
2020-04-06 11:04:59 -0400

    Change GameObj position

--- Unity Survival Guide/Assets/Player.cs
+++ Unity Survival Guide/Assets/Player.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public Vector3 startPosition;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //grab current position - assign new position
+        transform.position = startPosition;
     }
 
     // Update is called once per frame
